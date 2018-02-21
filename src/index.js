59a3a439b28d206a29cb83d74deb90343c1189d4
k3ep3r <keeper3211@gmail.com>
2018-02-21 11:51:26 +0300

    All test a passed

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.array = []; //Creating array list
+        this.compareFunction = (left,right) => left - right; // Custom comparator rules
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element); //need return for pushing elements to array;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index]; //return array index;
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length; //return getter of array length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.array; // This method need to return on link this object;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        //method for sort arrays
+        var sortArray = [];
+        indices.sort().forEach(j => sortArray.push(this.array[j]));
+        sortArray.sort(this.compareFunction);  //custom comparator sort
+        indices.forEach((j, i) => this.array[j] = sortArray[i]);
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
